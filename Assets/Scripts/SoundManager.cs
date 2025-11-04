788467f767a4333684096f66448bdf42fbcea62c
Brandon <brandonhutchison6@gmail.com>
2025-11-03 19:44:48 -0500

    SoundManager Script
    Added Functionality to change volume of Sounds by Slider with Sound Manager script

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource Music;
+    public AudioSource[] Sounds;
+    public Slider musicSlider;
+    public Slider soundSlider;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Music.volume *= musicSlider.value;
+        for (int i = 0; i < Sounds.Length; i++)
+        {
+            Sounds[i].volume *= soundSlider.value;
+        }  
+    }
+}
