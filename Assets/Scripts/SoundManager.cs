c21c3cd09fa0dd02fe424de08768b8188cb4bb72
Brandon <brandonhutchison6@gmail.com>
2025-11-03 20:28:07 -0500

    Added Save Serialization public function

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,23 +1,58 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEditor.Overlays;
 
 public class SoundManager : MonoBehaviour
 {
-    public AudioSource Music;
-    public AudioSource[] Sounds;
     public Slider musicSlider;
     public Slider soundSlider;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public AudioSource Music;
+    public AudioSource[] Sounds;
+    public GameObject saveMenu;
+    public bool saveChanges;
+
+    public void SaveGame()
     {
-        
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file;
+
+        //If the files exists open it.
+        if(File.Exists(Application.persistentDataPath + "/menudata.dat"))
+        {
+            file = File.Open(Application.persistentDataPath + "/menudata.dat", FileMode.Open):
+        }
+        //Else create it.
+        else
+        {
+            file = File.Create(Application.persistentDataPath + "/menudata.dat"):
+        }
+        //Write Data.
+        MenuData data = new MenuData();
+        data.savedMusicValue = musicSlider.value;
+        data.savedSoundValue = soundSlider.value;
+
+        //Write Data.
+        bf.Serialize(file, data);
+        //Close
+        file.Close();
+
+    }
+
+    [Serializable]
+    public class MenuData
+    {
+        public float savedMusicValue;
+        public float savedSoundValue;
     }
 
     // Update is called once per frame
     void Update()
     {
         Music.volume = musicSlider.value;
-        for (int i = 0; i < Sounds.Length; i++)
+        foreach (AudioSource i in Sounds)
         {
             Sounds[i].volume = soundSlider.value;
         }  
