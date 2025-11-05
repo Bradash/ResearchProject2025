c94bc53610dd2c5cfc548eb13ccb1aee6494e9d3
Brandon <brandonhutchison6@gmail.com>
2025-11-05 13:05:12 -0500

    Added Comments to SoundManager Script

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -29,16 +29,20 @@ public class SoundManager : MonoBehaviour
     {
         saveMenu.SetActive(true);
     }
-
+    //called by pressing Yes
     public void SaveGame()
     {
+        //grab public Class
         MenuData data = new MenuData();
+        //set public class values from client side values
         data.savedMusicValue = musicSlider.value;
         data.savedSoundValue = soundSlider.value;
-
+        //make public class to json
         string json = JsonUtility.ToJson(data);
+        //set path with writer
         using(StreamWriter writer = new StreamWriter(Application.persistentDataPath + "menudata.json"))
         {
+            //Serialize and Write file
             writer.Write(json);
         }
 
@@ -49,19 +53,22 @@ public class SoundManager : MonoBehaviour
 
     public void LoadGame()
     {
+            //string for json
             string json;
-
+            //set path with reader
             using(StreamReader reader = new StreamReader(Application.persistentDataPath + "menudata.json"))
             {
+            //Deserialize to string
                 json = reader.ReadToEnd();
             }
-
+            //convert string json to public class
             MenuData data = JsonUtility.FromJson<MenuData>(json);
+            //set client values public class values
             musicSlider.value = data.savedMusicValue;
             soundSlider.value = data.savedSoundValue;
             Debug.Log(data.savedMusicValue);
     }
-
+    //called by pressing no
     public void dontSave() 
     {
         Time.timeScale = 1;
@@ -77,7 +84,7 @@ public class SoundManager : MonoBehaviour
         public float savedSoundValue;
     }
 
-    // Update is called once per frame
+    // Called by touching sliders
     public void UpdateVolumes()
     {
         Music.volume = musicSlider.value;
