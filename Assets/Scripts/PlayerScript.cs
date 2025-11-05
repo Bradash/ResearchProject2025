2f952f6c42aec5d573f269c797f42e44c6d7a655
Brandon <brandonhutchison6@gmail.com>
2025-11-05 13:47:43 -0500

    Added Player Rotation to Character

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,18 +5,27 @@ using static SoundManager;
 public class PlayerScript : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
+    public Rigidbody2D rb;
     private void Start()
     {
         LoadColor();
     }
-    public Rigidbody2D rb;
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        playerFaceMouse();
         Vector2 moveDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        rb.MovePosition(rb.position + (moveDirection * 5 * Time.fixedDeltaTime));
+        rb.MovePosition(rb.position + (moveDirection * 5 * Time.deltaTime));
     }
+    void playerFaceMouse()
+    {
+        //make a vector that grabs the mouse position
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
+        //make a vector that's based on where the mouse is from the player
+        Vector2 dir = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        transform.up = dir;
+    }
 
     public void LoadColor()
     {
