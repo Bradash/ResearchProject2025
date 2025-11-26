473277b6c82acb2b2a8b42498e3bc3eaadb9efec
Brandon <39204861+Bradash@users.noreply.github.com>
2025-11-25 20:03:07 -0500

    Attempt 1 to add Object Pooling

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,20 +3,14 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public ItemType item;
-    float range;
     public float speed;
     void Update()
     {
         transform.Translate(transform.up * Time.deltaTime * speed, Space.World);
-        range += Time.deltaTime;
-        if (range > item.Range)
-        {
-            Destroy(gameObject);
-        }
     }
     
     private void OnBecameInvisible()
     {
-        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,13 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Weapon : MonoBehaviour
 {
+    public ObjectPool<GameObject> projectilePool;
     public ItemType currentItem;
     public GameObject projectile;
     SpriteRenderer weaponSprite;
     public Transform playerTransform;
     public GameObject trail;
 
+    private void Awake()
+    {
+        projectilePool = new ObjectPool<GameObject>(CreatePooledObject, OnGetFromPool, OnReturnToPool, OnDestroyPooledObject, false, 5, 10);
+    }
+    GameObject CreatePooledObject()
+    {
+        GameObject projectileNew = Instantiate(projectile, transform.position, playerTransform.rotation);
+        var projectileScript = projectileNew.GetComponent<Projectile>();
+        if (currentItem != null)
+        {
+            projectileScript.item = currentItem;
+        }
+        return projectileNew;
+    }
+    void OnGetFromPool(GameObject pooledObject)
+    {
+        pooledObject.SetActive(true);
+    }
+    void OnReturnToPool(GameObject pooledObject)
+    {
+        pooledObject.SetActive(false);
+    }
+    void OnDestroyPooledObject(GameObject pooledObject)
+    {
+        Destroy(pooledObject);
+    }
+    IEnumerator ReturnAfter(GameObject pooledObject)
+    {
+        yield return new WaitForSeconds(currentItem.Range);
+        // Give it back to the pool.
+        projectilePool.Release(pooledObject);
+    }
     private void Start()
     {
         weaponSprite = GetComponent<SpriteRenderer>();
@@ -31,16 +67,8 @@ public class Weapon : MonoBehaviour
             {
                 if (Input.GetMouseButtonDown(0))
                 {
-                    GameObject projectileNew = Instantiate(projectile, transform.position, playerTransform.rotation);
-                    var projectileScript = projectileNew.GetComponent<Projectile>();
-                    if (currentItem != null)
-                    {
-                        projectileScript.item = currentItem;
-                    }
-                    else 
-                    {
-                        Debug.LogError("Projectile is shot from no ItemType");
-                    }
+                    projectilePool.Get();
+                    ReturnAfter();
                 }
             }
             else
@@ -58,5 +86,15 @@ public class Weapon : MonoBehaviour
                 trail.SetActive(false);
             }
         }
-    }
+
+}
+        
+  
+
+
+
+
+
+
+
 }
