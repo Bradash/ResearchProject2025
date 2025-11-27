05f0700fbccf83635be50b14a4d030f596f718e9
Brandon <39204861+Bradash@users.noreply.github.com>
2025-11-26 22:05:39 -0500

    Fixed the Pickup Manager and named the proper scripts and references

--- Assets/Scripts/PickupManager.cs
+++ Assets/Scripts/PickupManager.cs
@@ -10,19 +10,22 @@ public class PickupManager : MonoBehaviour
         //for every pickup
         for (int s = 0; s < pickups.Count; s++)
         {
-            if(pickups[s] == null)
+            //if it's missing remove it (remove error When you pick up the item and load)
+            if (pickups[s] == null)
             {
                 pickups.RemoveAt(s);
             }
             //search the scriptableObject in the gameObject
-            InventoryItem pickupItem = pickups[s].GetComponent<InventoryItem>();
+            Pickup pickupItem = pickups[s].GetComponent<Pickup>();
             //for every item in inventory   
             for (int i = 0; i < inventory.inventoryItems.Length; i++)
             {
                 //if a pickup is the same as an item in the inventory, destroy it.
-                if (inventory.inventoryItems[i] == pickupItem)
+                if (inventory.inventoryItems[i].Item == pickupItem.item)
                 {
+                    //After Destroying remove it from the list
                     Destroy(pickups[s]);
+                    pickups.RemoveAt(s);
                 }
             }
         }
